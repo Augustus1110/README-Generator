3963f02c4c16e8797147cb23d41d2a7089d849c2
Augustus1110 <donahi22@gmail.com>
2023-07-07 16:49:35 -0700

    Tutor session - linked files, made Q&As, and badge

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,42 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of you project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please leave a brief description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your project?',
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license would you like to use?",
+        choices: ["MIT", "Apache", "None"]
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        console.log(answers)
+        fs.writeFileSync('testReadMe.md', generateMarkdown(answers))
+    });
+}
 
 // Function call to initialize app
 init();
---
+++ Develop/testReadMe.md
@@ -0,0 +1,11 @@
+# README
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  # Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Third Example](#third-example)
+## Description
+OMG
+
+## Installation
+You know how
